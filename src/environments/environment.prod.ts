98d1ad2a6a996f5b4e4167035f43d716dafb8d55
michael-otieno <otieno.oduor.michael@gmail.com>
2025-03-11 22:50:30 +0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production:true,
-  baseUrl:'http://172.18.0.2:8000/'
+  baseUrl:'http://172.18.0.3:8000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production:false,
-  baseUrl:'http://172.18.0.2:8000/'
+  // baseUrl:'http://172.18.0.3:8000/'
 
-  // baseUrl:'http://127.0.0.1:8000/'
+  baseUrl:'http://127.0.0.1:8000/'
 
 };
 
