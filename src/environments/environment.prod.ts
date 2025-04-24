dfe58934e825673848ce2c271d4de141d5971750
michael-otieno <otieno.oduor.michael@gmail.com>
2025-04-24 16:49:48 +0300

    fix: update environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production:true,
-  baseUrl:'http://127.0.0.1:8001/'
+  // baseUrl:'http://127.0.0.1:8001/'
+  baseUrl:'http://localhost.1:8001/'
+
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production:false,
-  // baseUrl:'http://172.18.0.3:8000/'
+  // baseUrl:'http://172.18.0.3:8000/'http://localhost:8001/login/
 
-  baseUrl:'http://127.0.0.1:8001/'
+  baseUrl:'http://localhost.1:8001/'
 
 };
 
