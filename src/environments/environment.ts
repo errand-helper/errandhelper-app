c31c4b7d09bb8ea4f89e8ab9b414417a490f5940
michael-otieno <otieno.oduor.michael@gmail.com>
2025-06-18 06:25:31 +0300

    update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production:false,
-  baseUrl:'http://localhost:8001/'
+  baseUrl:'http://192.168.59.108:30133/'
+  // baseUrl:'http://localhost:8001/'
 
   // baseUrl:'http://localhost.1:8001/'
 
