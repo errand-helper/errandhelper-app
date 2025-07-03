50a5766eec3e68410872174f4900e94da9863b08
michael-otieno <otieno.oduor.michael@gmail.com>
2025-07-03 15:18:37 +0300

    remove signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,12 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DecimalPipe } from '@angular/common';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-import { SignupComponent } from './apps/auth/signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    SignupComponent,
 
   ],
   imports: [
