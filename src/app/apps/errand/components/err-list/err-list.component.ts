e25555672c1260fa7a46fca4adad0cdc3145af74
michael-otieno <otieno.oduor.michael@gmail.com>
2025-10-20 07:59:54 +0300

    display errand list

--- src/app/apps/errand/components/err-list/err-list.component.ts
+++ src/app/apps/errand/components/err-list/err-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ErrandService } from '../../services/errand.service';
 
 @Component({
   selector: 'app-err-list',
   templateUrl: './err-list.component.html',
   styleUrl: './err-list.component.css'
 })
-export class ErrListComponent {
+export class ErrListComponent implements OnInit {
+
+  errands:any;
+  isClient:any;
+
+  constructor(private _errandService:ErrandService){}
+
+
+  ngOnInit(): void {
+    this.getErrands()
+    this.isClient = JSON.parse(localStorage.getItem('user_type') || '""');
+    console.log(this.isClient)
+  }
+
+  getErrands(){
+    this._errandService.getErrands().subscribe((res:any)=>{
+      console.log(res);
+      this.errands = res.results
+
+    })
+  }
+
+
 
 }
