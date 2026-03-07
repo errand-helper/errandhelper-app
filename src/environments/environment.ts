21f27cdf7602add7d869a687c35946a2d9667bf1
michael-otieno <otieno.oduor.michael@gmail.com>
2026-03-07 12:31:08 +0300

    update environment files for production deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://err-webapp-ingress-1241516008.us-east-1.elb.amazonaws.com/api/',
+  baseUrl: 'http://err-webapp-ingress-540763977.us-east-1.elb.amazonaws.com/api/',
   wsUrl: 'ws://192.168.49.2:30108/'
 };
 
