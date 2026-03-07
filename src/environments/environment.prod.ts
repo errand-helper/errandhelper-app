a39c5a370d6a9f1286d917d006063c8de8f8cced
michael-otieno <otieno.oduor.michael@gmail.com>
2026-03-07 11:37:39 +0300

    update environment files for production deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production:true,
-  baseUrl:'api/'
+  baseUrl:'http://err-webapp-ingress-1241516008.us-east-1.elb.amazonaws.com/api/',
+  // wsUrl: 'ws://err-webapp-ingress-1241516008.us-east-1.elb.amazonaws.com/ws/'
 
   // baseUrl:'http://localhost:8080/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://192.168.49.2:30108/',
+  baseUrl: 'http://err-webapp-ingress-1241516008.us-east-1.elb.amazonaws.com/api/',
   wsUrl: 'ws://192.168.49.2:30108/'
 };
 
